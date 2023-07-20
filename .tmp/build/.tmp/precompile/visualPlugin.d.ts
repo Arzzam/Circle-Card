e8fea4342b7c70f99a3ccc4744202f8ad28fc36d
Nafil Arzzam <92790024+Arzzam@users.noreply.github.com>
2023-07-20 13:11:51 +0530

    Delete .tmp

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var circleCard394AECA03C5B4F7CA16142CD2F6B048E_DEBUG: IVisualPlugin;
-export default circleCard394AECA03C5B4F7CA16142CD2F6B048E_DEBUG;
